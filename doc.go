d9d6d9e27bb8ebcc028825645aabc27b8cdae388
Cheikh Seck <jvmstix@gmail.com>
2022-08-05 13:09:04 +0000

    Update doc.go

--- doc.go
+++ doc.go
@@ -23,6 +23,9 @@ func AweSomeFactory() Awesome {
 
 // Awesome is awesome
 // To instantiate, refer to [medium_examples.AweSomeFactory] 
+// Remember to [connect] first
+//
+// [connect]: medium_examples.ConnectToTCP
 type Awesome struct {
 	Ids []string
 }
