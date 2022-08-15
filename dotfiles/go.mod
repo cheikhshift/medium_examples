31197cab8198c4c232c433844b277f39e491b188
Cheikh Seck <jvmstix@gmail.com>
2022-08-15 19:33:21 +0000

    Add files via upload

---
+++ dotfiles/go.mod
@@ -0,0 +1,5 @@
+module dotfiles
+
+go 1.18
+
+require mvdan.cc/sh/v3 v3.5.1
---
+++ dotfiles/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"mvdan.cc/sh/v3/syntax"
+)
+
+func main() {
+
+	os.Clearenv()
+	input, err := os.Open(".config")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer input.Close()
+
+
+	parser := syntax.NewParser()
+	file, err := parser.Parse(input, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	printer := syntax.NewPrinter()
+
+	syntax.Walk(file, func(node syntax.Node) bool {
+
+		switch x := node.(type) {
+		case *syntax.DeclClause:
+
+			if x.Variant.Value == "export" {
+
+				var b bytes.Buffer
+
+				name := x.Args[0].Name.Value
+				
+				printer.Print(&b, x.Args[0].Value)
+
+				if b.Len() > 0 {
+
+					trimmed := strings.Trim(b.String(), "\"")
+
+					fmt.Println(x.Args[0].Name.Value, " =", trimmed)
+
+					v := os.ExpandEnv(trimmed)
+
+					err := os.Setenv(name, v)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+
+			}
+
+		}
+
+		return true
+	})
+
+	fmt.Println(os.Environ())
+}
